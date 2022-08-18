a8fe1d90db36f0526e4b54052acc4f8235f23539
roilan.lambert5@gmail.com <roilan.lambert5@gmail.com>
2022-08-18 00:38:09 -0400

    prueba 1

---
+++ js/App.js
@@ -0,0 +1,49 @@
+let articles = document.querySelectorAll('article')
+let h1_footer = document.querySelector('.h1_footer')
+let h2_footer = document.querySelector('.h2_footer')
+let name = document.querySelector('.name').textContent = '</Roilan Lambert>'
+let profesion = document.querySelector('.profesion').textContent = '</Web Developer>'
+
+const cb = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting && entry.target.classList.contains('float-lg-start')){
+            entry.target.style.animationName = 'entry_left'
+        }
+        else if(!entry.isIntersecting && entry.target.classList.contains('float-lg-start')){
+            entry.target.style.animationName = 'exit_left'
+        }
+
+        if(entry.isIntersecting && entry.target.classList.contains('float-lg-end')){
+            entry.target.style.animationName = 'entry_right'
+        }
+        else if(!entry.isIntersecting && entry.target.classList.contains('float-lg-end')){
+            entry.target.style.animationName = 'exit_right'
+        }
+
+        if(entry.isIntersecting && entry.target.classList.contains('h1_footer') ){
+            entry.target.style.animationName = 'entry_left'
+            console.log(entry.target)
+        }
+        if(entry.target.classList.contains('h2_footer')){
+            entry.target.style.animationName = 'entry_left'
+            console.log(entry.target)
+        }
+        if(!entry.isIntersecting && entry.target.classList.contains('h1_footer')){
+            entry.target.style.animationName = 'exit_left'
+            console.log(entry.target)
+        }
+        if(!entry.isIntersecting && entry.target.classList.contains('h2_footer')){
+            entry.target.style.animationName = 'exit_left'
+            console.log(entry.target)
+        }
+        
+        
+    });
+}
+
+
+const observer = new IntersectionObserver(cb)
+articles.forEach(element => observer.observe(element))
+observer.observe(h1_footer)
+setTimeout(observer.observe(h2_footer),1000)
+
